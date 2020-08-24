42e4b7b4274cd8cbdd2dc33f88c71abd4a60144d
Joel Elliston <lachre@outlook.com>
2020-08-24 23:07:43 +0300

    finish game.rs

--- src/game.rs
+++ src/game.rs
@@ -120,7 +120,36 @@ impl Game {
     fn add_food(&mut self) {
         let mut rng = thread_rng();
 
-        let mut new_x = rng.
+        let mut new_x = rng.gen_range(1, self.width - 1);
+        let mut new_y = rng.gen_range(1, self.width - 1);
+        while self.snake.overlap_tail(new_x, new_y) {
+            new_x = rng.gen_range(1, self.width - 1);
+            new_y = rng.gen_range(1, self.width - 1);
+        }
+
+        self.food_x = new_x;
+        self.food_y = new_y;
+        self.food_exists = true;
+    }
+
+    fn update_snake(&mut self, dir: Option<Direction>) {
+        if self.check_if_snake_alive(dir) {
+            self.snake.move_forward(dir);
+            self.check_eating();
+        } else {
+            self.game_over = true;
+        }
+        self.watiting_time = 0.0;
+
+    }
+
+    fn restart(&mut self) {
+        self.snake = Snake::new(2, 2);
+        self.watiting_time = 0.0;
+        self.food_exists = true;
+        self.food_x = 6;
+        self.food_y = 4;
+        self.game_over = false;
     }
 
 }
